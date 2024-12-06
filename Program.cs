aa23723f3ad47eb9b956435fe173b1d75a9b03a0
JoshuaHartop <joshua.hartopp@gmail.com>
2024-12-06 12:53:07 -0400

    finished day 3 step 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Text.RegularExpressions;
+class AdventOfCodeDay3
+{
+    string instructions = @"..\..\..\TEXTFILE\LIST.txt";
+    string[] readInstructions = [];
+    List<string> readInstructions2 = [];
+    List<int> readInstructions3 = [];
+
+    static void Main()
+    {
+
+        AdventOfCodeDay3 program = new AdventOfCodeDay3();
+        int finalValues = 0;
+        program.readInstructions = File.ReadAllLines(program.instructions);
+        program.FindInstructions();
+        for (int i = 0; i < program.readInstructions3.Count; i++)
+        {
+            finalValues += program.readInstructions3[i];
+        }
+        Console.WriteLine(finalValues);
+    }
+    void FindInstructions()
+    {
+        bool acceptingMul = true;
+        readInstructions2 = new List<string>();
+        string instruction;
+        Regex regex = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)");
+        for (int i = 0; i < readInstructions.Length; i++)
+        {
+            foreach (Match match in regex.Matches(readInstructions[i]))
+            {
+                Console.WriteLine(match.Value);
+                if (match.Value.Contains("do()"))
+                {
+                    acceptingMul = true;
+                }
+                else if (match.Value.Contains("don't()"))
+                {
+                    acceptingMul = false;
+                }
+                else if (acceptingMul)
+                {
+                    instruction = Regex.Replace(match.Value, @"mul\(", "");
+                    readInstructions2.Add(Regex.Replace(instruction, @"\)", ""));
+                }
+            }
+        }
+        
+        for (int i = 0; i < readInstructions2.Count; i++)
+        {
+                string[] splitInstructions = readInstructions2[i].Split(',');
+                readInstructions3.Add(Int32.Parse(splitInstructions[0]) * Int32.Parse(splitInstructions[1]));
+        }
+    }
+}
--- obj/Debug/net8.0/AdventOfCodeDay3Step2.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCodeDay3Step2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCodeDay3Step2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d245bc379d556130ceb427014c600dfada515d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCodeDay3Step2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCodeDay3Step2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
